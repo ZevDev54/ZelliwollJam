a642946f2bee5ece29c243b2d6edb9520bc335f0
soydevelop <elliottmelvin969@gmail.com>
2023-12-28 14:18:35 -0700

    Menu now connect to level

--- Main Menu.gd
+++ Main Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://___.tscn")
+	get_tree().change_scene_to_file("res://ScrollingLevel.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit
