dab46b9d3de0ec01a6275fcc42eb51f60091303d
soydevelop <elliottmelvin969@gmail.com>
2023-12-28 14:11:05 -0700

    Created Menu

--- Main Menu.gd
+++ Main Menu.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _on_play_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://___.tscn")
 
-
+func _on_quit_pressed():
+	get_tree().quit
 
