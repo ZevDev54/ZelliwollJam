e7072c0161934a9e1958972c898cdc223039bbf2
soydevelop <elliottmelvin969@gmail.com>
2023-12-29 16:00:03 -0700

    does not work

--- TopDownSnakeMovement.gd
+++ TopDownSnakeMovement.gd
@@ -1,8 +1,12 @@
 extends RigidBody2D
 
-@export var speed := 100;
-@export var slowDownDistance := 500;
 
+@export var slowDownDistance := 500;
+@export var time_held := 0
+var mouse_pos = get_global_mouse_position()
+var timer := Timer.new()
+
+var total = 0
 const MOVE_SPEED = 300
 
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +18,44 @@ func _ready():
 func _process(_delta):
 	pass
 
-func _physics_process(	delta):
+func _physics_process(delta):
 
 
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_pressed("Pull"):
+		total += delta
+		
 
+
+
+		
+	if Input.is_action_just_released("Pull"):
+		
+		
+		timer.connect("timeout", self, "stopPull")
+		timer.wait_time = total
+		add_child(timer)
+		var speed := 100;
+		timer.start()
+		
+		print("WORK")
+		
+		var velocity_vector = mouse_pos - global_position;
+		var distance = velocity_vector.length();
+	
+		var distAsPercent = distance / slowDownDistance;
+	
+		var slowdownMultiplier = clamp(distAsPercent, 0, 1);
+	
+		var normalized_velocity = velocity_vector.normalized()
+	
+		apply_central_force(normalized_velocity * speed * slowdownMultiplier);
+
+	
+	
+
+
+
+	if Input.is_action_just_pressed("Push"):
 		var mouse_pos = get_global_mouse_position()
 		var velocity_vector = mouse_pos - global_position;
 		var distance = velocity_vector.length();
@@ -29,7 +66,7 @@ func _physics_process(	delta):
 	
 		var normalized_velocity = velocity_vector.normalized()
 
-		apply_central_force(normalized_velocity * speed * slowdownMultiplier);
+		apply_central_force(normalized_velocity * (speed*100) * slowdownMultiplier * -1);
 
 
 	var move_vec = Vector2()
@@ -46,4 +83,6 @@ func _physics_process(	delta):
 	
 	#print(name+slowdownMultiplier);
 
+func stopPull():
+	speed = 0
 
