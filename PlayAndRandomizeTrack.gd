c39f4bf9ea4ee131b9e57c2023ad8c3c3d389280
ZevDev54 <zevy.johnston@gmail.com>
2023-12-29 17:59:44 -0700

    Worked on flutist music notes

--- PlayAndRandomizeTrack.gd
+++ PlayAndRandomizeTrack.gd
@@ -1,6 +1,7 @@
 extends AudioStreamPlayer2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func PlayRandomTrack()
+	
