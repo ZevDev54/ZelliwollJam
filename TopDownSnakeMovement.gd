28e48e231615fa9e09e404e089d3c7918f4987d4
ZevDev54 <zevy.johnston@gmail.com>
2023-12-30 10:21:32 -0700

    Added music cloud and changed snakes to move on mouse held. Fixed score display

--- TopDownSnakeMovement.gd
+++ TopDownSnakeMovement.gd
@@ -13,6 +13,10 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
+
+	if !Input.is_action_pressed("leftclick"):
+		return;
+
 	var mouse_pos = get_global_mouse_position()
 	var velocity_vector = mouse_pos - global_position;
 	var distance = velocity_vector.length();
@@ -22,10 +26,13 @@ func _physics_process(delta):
 	var slowdownMultiplier = clamp(distAsPercent, 0, 1);
 
 	var normalized_velocity = velocity_vector.normalized()
-	apply_central_force(normalized_velocity * speed * slowdownMultiplier);
+	apply_central_force(normalized_velocity * speed * slowdownMultiplier); 
+
+	#if distAsPercent < 1:
+	#	apply_central_force(normalized_velocity * -speed * 1/distAsPercent); 
+
 
 	#print(name+slowdownMultiplier);
-
 
 
 #only called when contacts hazard layer items.
